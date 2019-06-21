c293b6ff5b59d11d6b3af5859e5e5303d53c8bba
Brett Gutstein <brett.gutstein@cst.cam.ac.uk>
2019-06-21 15:08:19 +0100

    clean up

--- mrs.c
+++ mrs.c
@@ -37,7 +37,7 @@ void free(void *ptr) {
   return mrs_free(ptr);
 }
 
-static int insert_sh_desc();
+static int insert_shadow_desc();
 
 #define DEBUG 1
 
@@ -45,9 +45,10 @@ static int insert_sh_desc();
 #define mrs_debug_printf(fmt, ...) \
   do {if (DEBUG) fprintf(stderr, ("mrs: " fmt), ##__VA_ARGS__); } while (0)
 
+#define cheri_testsubset(x, y) __builtin_cheri_subset_test((x), (y))
 
 struct mrs_params {
-  struct mrs_sh_desc *shadow_spaces;
+  struct mrs_shadow_desc *shadow_spaces;
   RB_HEAD(mrs_alloc_desc_head, mrs_alloc_desc) allocations;
   void* (*real_mmap) (void*, size_t, int, int, int, off_t);
   void* (*real_malloc) (size_t);
@@ -60,44 +61,42 @@ static struct mrs_params params = {0};
 // TODO support removal and free list of descriptors, use better data
 // structures; this wants a red-black tree with coalescing (on insertion) and
 // splitting (on removal), but how can we do this nicely with capabilities?
+// XXX we may not actually need to handle coalescing or splitting, since
+// CHERIfied allocators won't allocate across mmap boundaries (and may not
+// munmap sub-regions)
 
-// TODO what if sub-mmapped-size regions are munmapped
-
-// TODO use testsubset with mmap capability?
-struct mrs_sh_desc {
-  struct mrs_sh_desc *next;
-  vaddr_t mmap_base;
-  size_t mmap_size;
-  void *sh;
-  // TODO count number of corresponding allocs
+struct mrs_shadow_desc {
+  struct mrs_shadow_desc *next;
+  void *mapped;
+  void *shadow;
+  // TODO count number of corresponding allocs to validate munmap
 };
 
-static const int NUM_SH_DESCS = 1000;
-static int sh_desc_index = 0;
-static struct mrs_sh_desc sh_descs[NUM_SH_DESCS];
+static const int NUM_SHADOW_DESCS = 1000;
+static int shadow_desc_index = 0;
+static struct mrs_shadow_desc shadow_descs[NUM_SHADOW_DESCS];
 
-static int insert_sh_desc(vaddr_t base, size_t size, void *sh) {
-  if (sh_desc_index == NUM_SH_DESCS) {
-    printf("insert_sh_desc: maximum number of sh_descs exceeded\n");
+static int insert_shadow_desc(void *mapped, void *shadow) {
+  if (shadow_desc_index == NUM_SHADOW_DESCS) {
+    printf("insert_shadow_desc: maximum number of shadow_descs exceeded\n");
     return -1;
   }
 
-  struct mrs_sh_desc *ins = &sh_descs[sh_desc_index];
-  sh_desc_index++;
+  struct mrs_shadow_desc *ins = &shadow_descs[shadow_desc_index];
+  shadow_desc_index++;
 
   ins->next = NULL;
-  ins->mmap_base = base;
-  ins->mmap_size = size;
-  ins->sh = sh;
+  ins->mapped = mapped;
+  ins->shadow = shadow;
 
   if (params.shadow_spaces == NULL) {
     params.shadow_spaces = ins;
   } else {
-    struct mrs_sh_desc *iter = params.shadow_spaces;
-    if (ins->mmap_base < iter->mmap_base) {
+    struct mrs_shadow_desc *iter = params.shadow_spaces;
+    if (cheri_getbase(ins->mapped) < cheri_getbase(iter->mapped)) {
       ins->next = iter;
       params.shadow_spaces = ins;
-    } else while ((iter->next != NULL) && (ins->mmap_base > iter->next->mmap_base)) {
+    } else while ((iter->next != NULL) && (cheri_getbase(ins->mapped) > cheri_getbase(iter->next->mapped))) {
       iter = iter->next;
     }
     ins->next = iter->next;
@@ -107,11 +106,14 @@ static int insert_sh_desc(vaddr_t base, size_t size, void *sh) {
   return 0;
 }
 
-static void *lookup_sh_desc(vaddr_t alloc_base, size_t alloc_size) {
-  struct mrs_sh_desc *iter = params.shadow_spaces;
+/**
+ * Given a capability returned by malloc, return the shadow descriptor for its
+ * corresponding shadow space.
+ **/
+static void *lookup_shadow_desc(void *alloc) {
+  struct mrs_shadow_desc *iter = params.shadow_spaces;
   while (iter != NULL) {
-    // TODO what if an allocation spans multiple mmaped regions?
-    if (((iter->mmap_base + iter->mmap_size) >= (alloc_base + alloc_size)) && (iter->mmap_base <= alloc_base)) {
+    if (cheri_testsubset(iter->mapped, alloc)) {
       return iter;
     }
     iter = iter->next;
@@ -124,17 +126,14 @@ static void *lookup_sh_desc(vaddr_t alloc_base, size_t alloc_size) {
 // ------------------- ALLOCATED REGIONS
 // TODO support free list of descriptors
 
-// TODO use testsubset with malloc capability?
 struct mrs_alloc_desc {
-  RB_ENTRY(mrs_alloc_desc) linkage;
   void *alloc;
-  vaddr_t alloc_base; // redundancy so we can RB_FIND without a capability
-  size_t alloc_size;
-  struct mrs_sh_desc *sh_desc;
+  struct mrs_shadow_desc *shadow_desc;
+  RB_ENTRY(mrs_alloc_desc) linkage;
 };
 
 static vaddr_t mrs_alloc_desc_cmp(struct mrs_alloc_desc *e1, struct mrs_alloc_desc *e2) {
-  return e1->alloc_base - e2->alloc_base;
+  return cheri_getbase(e1->alloc) - cheri_getbase(e2->alloc);
 }
 
 RB_PROTOTYPE(mrs_alloc_desc_head, mrs_alloc_desc, linkage, mrs_alloc_desc_cmp);
@@ -144,25 +143,28 @@ static const int NUM_ALLOC_DESCS = 1000;
 static int alloc_desc_index = 0;
 static struct mrs_alloc_desc alloc_descs[NUM_ALLOC_DESCS];
 
-static struct mrs_alloc_desc *insert_alloc_desc(void *alloc, struct mrs_sh_desc *sh_desc) {
+static int insert_alloc_desc(void *alloc, struct mrs_shadow_desc *shadow_desc) {
   if (alloc_desc_index == NUM_ALLOC_DESCS) {
     printf("insert_alloc_desc: maximum number of alloc_descs exceeded\n");
-    return NULL; // TODO null is a legitimate return value for RB_INSERT
+    return -1;
   }
 
   struct mrs_alloc_desc *ins = &alloc_descs[alloc_desc_index];
   alloc_desc_index++;
   ins->alloc = alloc;
-  ins->alloc_base = cheri_getbase(alloc);
-  ins->alloc_size = cheri_getlen(alloc);
-  ins->sh_desc = sh_desc;
+  ins->shadow_desc = shadow_desc;
 
-  return RB_INSERT(mrs_alloc_desc_head, &params.allocations, ins);
+  if (RB_INSERT(mrs_alloc_desc_head, &params.allocations, ins)) {
+    printf("insert_alloc_desc: duplicate insert\n");
+    return -1;
+  }
+
+  return 0;
 }
 
-static struct mrs_alloc_desc *lookup_alloc_desc(vaddr_t base) {
-  struct mrs_alloc_desc lookup = {{0}}; //TODO
-  lookup.alloc_base = base;
+static struct mrs_alloc_desc *lookup_alloc_desc(void *alloc) {
+  struct mrs_alloc_desc lookup = {0};
+  lookup.alloc = alloc;
   return RB_FIND(mrs_alloc_desc_head, &params.allocations, &lookup);
 }
 
@@ -181,19 +183,19 @@ void *mrs_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset
     mrs_debug_printf("mrs_mmap: error in mmap errno %d\n", errno);
     return MAP_FAILED;
   }
-  mrs_debug_printf("mrs_mmap: mmap returned %p\n", mb);
 
   void *sh;
   if (caprevoke_shadow(CAPREVOKE_SHADOW_NOVMMAP, mb, &sh)) {
     mrs_debug_printf("mrs_mmap: error in caprevoke_shadow errno %d\n", errno);
     return MAP_FAILED;
   }
-  mrs_debug_printf("mrs_mmap: caprevoke_shadow returned 0x%zx\n", cheri_getaddress(sh));
 
-  if (insert_sh_desc(cheri_getbase(mb), cheri_getlen(mb), sh)) {
+  if (insert_shadow_desc(mb, sh)) {
     mrs_debug_printf("mrs_mmap: recording newly mapped region\n");
     return MAP_FAILED;
   }
+
+  mrs_debug_printf("mrs_mmap: mmap returned %p caprevoke_shadow returned %p\n", mb, sh);
 
   return mb;
 }
@@ -206,6 +208,10 @@ void *mrs_malloc(size_t size) {
     // TODO dlsym might use malloc
     params.real_malloc = dlsym(RTLD_NEXT, "malloc");
   }
+  if (params.real_free == NULL) {
+    // TODO dlsym might use free
+    params.real_free = dlsym(RTLD_NEXT, "free");
+  }
 
   void *alloc = params.real_malloc(size);
 
@@ -216,19 +222,22 @@ void *mrs_malloc(size_t size) {
    *
    */
 
-  void *sh = lookup_sh_desc(cheri_getbase(alloc), cheri_getlen(alloc));
+  void *sh = lookup_shadow_desc(alloc);
   if (sh == NULL) {
     mrs_debug_printf("mrs_malloc: looking up shadow space failed\n");
-    // TODO make sure this has been loaded params.real_free(alloc);
+    params.real_free(alloc);
     return NULL;
   }
 
-  insert_alloc_desc(alloc, sh);
+  if (insert_alloc_desc(alloc, sh)) {
+    mrs_debug_printf("mrs_malloc: inserting alloc descriptor failed\n");
+    params.real_free(alloc);
+    return NULL;
+  }
 
   mrs_debug_printf("mrs_malloc: called size 0x%zx address %p\n", cheri_getlen(alloc), alloc);
 
   return alloc;
-
 }
 
 void mrs_free(void *ptr) {
@@ -243,20 +252,21 @@ void mrs_free(void *ptr) {
     params.real_free = dlsym(RTLD_NEXT, "free");
   }
 
-  struct mrs_alloc_desc *alloc = lookup_alloc_desc(cheri_getbase(ptr));
+  struct mrs_alloc_desc *alloc = lookup_alloc_desc(ptr);
 
   if (alloc == NULL) {
     mrs_debug_printf("mrs_free: freed base address not allocated\n");
     exit(7);
   }
 
-  if (cheri_getlen(ptr)!= alloc->alloc_size) {
-    mrs_debug_printf("mrs_free: freed base address size mismatch cap len 0x%zx alloc size 0x%zx\n", cheri_getlen(ptr), alloc->alloc_size);
+  if (cheri_getlen(ptr)!= cheri_getlen(alloc->alloc)) {
+    mrs_debug_printf("mrs_free: freed base address size mismatch cap len 0x%zx alloc size 0x%zx\n", cheri_getlen(ptr), cheri_getlen(alloc->alloc));
     exit(7);
   }
 
   /* TODO
    *
+   * - better validation
    * - add it to the quarantine list, use data structures to set appropriate shadow bitmap
    * - free descriptor
    * - if quarantine full, run revoker and free memory
